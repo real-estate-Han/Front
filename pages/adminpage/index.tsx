0315ad725d2f94a707e5de5d62a102798d90057c
momoco-git <gnspope@naver.com>
2023-02-23 13:19:35 +0900

    pix error empty

---
+++ pages/adminpage/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { css } from '@emotion/react';
+import { Map } from 'react-kakao-maps-sdk';
+export default function Detail() {
+  return (
+    <div>
+      <div>어드민페이지</div>
+    </div>
+  );
+}
