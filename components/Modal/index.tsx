624aaa9ab2ffd18f2ea8dc7742b96fdbba3864b8
hotsix <codinggo@naver.com>
2023-03-23 23:54:27 +0900

    Feat login component create login component by @waterfist #1

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,30 +1,35 @@
-import React, { useEffect } from 'react';
-import styled from '@emotion/styled';
+import React, { useEffect } from "react";
+import styled from "@emotion/styled";
+import useStore from "@zustand/store";
 
 interface ModalProps {
   children: React.ReactNode;
   closeModal: () => void;
 }
-const Modal = (props: ModalProps) => {
- 
 
-  function closeModal() {}
-  // 모달창이 나왔을때 백그라운드 클릭이 안되게 하고 스크롤도 고정하는 방법
-  useEffect(() => {
-    document.body.style.cssText = `
-      position: fixed; 
-      top: -${window.scrollY}px;
-      overflow-y: scroll;
-      width: 100%;`;
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY) * -1);
-    };
-  }, []);
+const Modal = (props: ModalProps) => {
+  const { modalState, changeModalState } = useStore((state) => state);
+
+  function closeModal() {
+    // 모달창이 나왔을때 백그라운드 클릭이 안되게 하고 스크롤도 고정하는 방법
+    useEffect(() => {
+      const html = document.documentElement;
+      if (modalState) {
+        html.style.overflowY = "hidden";
+        html.style.overflowX = "hidden";
+      } else {
+        html.style.overflowY = "auto";
+        html.style.overflowX = "auto";
+      }
+      return () => {
+        html.style.overflowY = "auto";
+        html.style.overflowX = "auto";
+      };
+    }, [modalState]);
+  }
 
   return (
-    <ModalStyled onClick={props.closeModal}>
+    <ModalStyled onClick={changeModalState}>
       <div className="modalBody" onClick={(e) => e.stopPropagation()}>
         {props.children}
       </div>
